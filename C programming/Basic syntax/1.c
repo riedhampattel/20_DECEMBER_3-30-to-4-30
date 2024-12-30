60af64ed1d1c448b5974167b4740ac4eb61f9f60
riedhampattel <riedhampattel.tops@gmail.com>
2024-12-30 16:30:13 +0530

    Add files via upload

---
+++ C programming/Basic syntax/1.c
@@ -0,0 +1,16 @@
+//Single line comment
+/*
+	Multilin comment
+	# ---> pre processor
+	include ---> directive
+	stdio.h ---> standard input output header file
+	int ---> return type
+	main ---> function name
+*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello Students!");
+	printf("\nHow are you?");
+	return 0;
+}
