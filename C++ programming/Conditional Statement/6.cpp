680cf16480bf17d4ef417bb28c34acea9e0185a8
riedhampattel <riedhampattel.tops@gmail.com>
2025-01-31 16:32:54 +0530

    updated

--- C++ programming/Conditional Statement/6.cpp
+++ C++ programming/Conditional Statement/6.cpp
@@ -5,3 +5,25 @@ Test Data :
 Expected Output :
 This is a special character.
 */
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"\nEnter any character = ";
+	cin>>ch;
+	
+	if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z'))
+	{
+		cout<<"\nYou have enetered an alphabet";
+	}
+	else if(ch>='0' && ch<='9')
+	{
+		cout<<"\nYou have enetered a number";
+	}
+	else
+	{
+		cout<<"\nYou have enetered a special character";
+	}
+	return 0;
+}
